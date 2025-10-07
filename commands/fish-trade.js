0e2a276596c0b9ac8c206f864fc49532576ec65d
Minebox260 <joellascoumes@gmail.com>
2025-10-07 03:22:16 +0200

    fix: trade safeguards and wrapping in a transaction

--- commands/fish-trade.js
+++ commands/fish-trade.js
@@ -56,12 +56,20 @@ module.exports = {
         }
     },
     async execute(interaction) {
-        const {db, cache} = require('../index.js');
+        const {db, cache, sequelize} = require('../index.js');
         const list = interaction.options.getString('list');
         const user = interaction.user;
         const targetUser = interaction.options.getUser('user');
         const levelToGive = interaction.options.getString('leveltogive');
         const levelToGet = interaction.options.getString('leveltoget');
+
+        if (user.id === targetUser.id) {
+            return await interaction.reply({content: ':x: You cannot trade with yourself', ephemeral: true});
+        }
+
+        if (levelToGive === levelToGet) {
+            return await interaction.reply({content: ':x: You cannot trade the same level', ephemeral: true});
+        }
 
         if (!(await interaction.client.guilds.cache.get(interaction.guildId).members.fetch(targetUser.id))) {
             return await interaction.reply({content: 'This user is not in this server', ephemeral: true});
@@ -77,18 +85,15 @@ module.exports = {
         if (!targetData) {
             return await interaction.reply({content: ':x: This user does not have any fishing data for this list', ephemeral: true});
         }
+
         const userFishedList = JSON.parse(userData.fished_list);
-        const userFishedListFrequency = JSON.parse(userData.fished_list_frequency);
-
         const targetFishedList = JSON.parse(targetData.fished_list);
-        const targetFishedListFrequency = JSON.parse(targetData.fished_list_frequency);
 
-        const userLevelIndex = userFishedList.findIndex(level => level === levelToGive);
-        if (userLevelIndex === -1) {
+        if (!userFishedList.includes(levelToGive)) {
             return await interaction.reply({content: ':x: You do not have fished this level yet', ephemeral: true});
         }
-        const targetLevelIndex = targetFishedList.findIndex(level => level === levelToGet);
-        if (targetLevelIndex === -1) {
+
+        if (!targetFishedList.includes(levelToGet)) {
             return await interaction.reply({content: ':x: This user does not have fished this level yet', ephemeral: true});
         }
 
@@ -111,52 +116,88 @@ module.exports = {
             .setColor("Red");
 
         const editRow = new ActionRowBuilder().addComponents(confirmTrade, cancelTrade);
-        const tradeMessage = await interaction.reply({ content: `<@${targetUser.id}>`, components: [editRow], embeds: [embed]});
+        const tradeMessage = await interaction.reply({ content: `<@${targetUser.id}>`, components: [editRow], embeds: [embed], fetchReply: true });
 
         const tradeCollectorFilter = i => i.user.id === targetUser.id;
         try {
             const tradeConfirmation = await tradeMessage.awaitMessageComponent({ filter: tradeCollectorFilter, time: 120_000 });
 
+            // Disable further interactions
+            const disabledRow = new ActionRowBuilder().addComponents(confirmTrade.setDisabled(true), cancelTrade.setDisabled(true));
+            await tradeConfirmation.update({ components: [disabledRow] });
+
+
+            // Process trade
             if (tradeConfirmation.customId === 'confirmTrade') {
-                // Removing levels
-                if (userFishedListFrequency[userLevelIndex] === 1) {
-                    userFishedList.splice(userLevelIndex, 1);
-                    userFishedListFrequency.splice(userLevelIndex, 1);
-                } else {
-                    userFishedListFrequency[userLevelIndex]--;
+
+                const transaction = await sequelize.transaction({ isolationLevel: 'SERIALIZABLE' });
+                try {
+
+                    // Reload fresh user data with lock
+                    const [userData, targetData] = await Promise.all([
+                        db[list].findOne({ where: { user: user.id }, lock: transaction.LOCK.UPDATE, transaction: transaction }),
+                        db[list].findOne({ where: { user: targetUser.id }, lock: transaction.LOCK.UPDATE, transaction: transaction })
+                    ]);
+
+                    const userFishedList = JSON.parse(userData.fished_list);
+                    const userFishedListFrequency = JSON.parse(userData.fished_list_frequency);
+                    const targetFishedList = JSON.parse(targetData.fished_list);
+                    const targetFishedListFrequency = JSON.parse(targetData.fished_list_frequency);
+
+                    // Re-check if users still have the levels
+                    const userLevelIndex = userFishedList.findIndex(level => level === levelToGive);
+                    const targetLevelIndex = targetFishedList.findIndex(level => level === levelToGet);
+                    if (userLevelIndex === -1 || targetLevelIndex === -1) {
+                        await transaction.rollback();
+                        return await interaction.editReply({components: [], embeds:[new EmbedBuilder().setDescription(`${tradeText}\n:x: Trade failed, one of the levels is no longer available`).setColor("Red")]});
+                    }
+
+                    // Removing levels
+                    if (--userFishedListFrequency[userLevelIndex] === 0) {
+                        userFishedList.splice(userLevelIndex, 1);
+                        userFishedListFrequency.splice(userLevelIndex, 1);
+                    }
+                    if (--targetFishedListFrequency[targetLevelIndex] === 0) {
+                        targetFishedList.splice(targetLevelIndex, 1);
+                        targetFishedListFrequency.splice(targetLevelIndex, 1);
+                    }
+
+                    // Adding new levels
+                    const userNewLevelIndex = userFishedList.findIndex(level => level === levelToGet);
+                    const targetNewLevelIndex = targetFishedList.findIndex(level => level === levelToGive);
+
+                    if (userNewLevelIndex === -1 ) {
+                        userFishedList.push(levelToGet);
+                        userFishedListFrequency.push(1);
+                    } else {
+                        userFishedListFrequency[userNewLevelIndex]++;
+                    }
+
+                    if (targetNewLevelIndex === -1) {
+                        targetFishedList.push(levelToGive);
+                        targetFishedListFrequency.push(1);
+                    } else {
+                        targetFishedListFrequency[targetNewLevelIndex]++;
+                    }
+
+                    await Promise.all([
+                        db[list].update(
+                            { fished_list: JSON.stringify(userFishedList), fished_list_frequency: JSON.stringify(userFishedListFrequency) },
+                            { where: { user: user.id }, transaction }
+                        ),
+                        db[list].update(
+                            { fished_list: JSON.stringify(targetFishedList), fished_list_frequency: JSON.stringify(targetFishedListFrequency) },
+                            { where: { user: targetUser.id }, transaction }
+                        ),
+                    ]);
+
+                    await transaction.commit();
+                    await interaction.editReply({components: [], embeds:[new EmbedBuilder().setDescription(`${tradeText}\n:white_check_mark: Trade successful`).setColor("Red")]});
+                } catch (_) {
+                    await transaction.rollback();
+                    return await interaction.editReply({components: [], embeds:[new EmbedBuilder().setDescription(`${tradeText}\n:x: An error occurred, please try again`).setColor("Red")]});
                 }
-
-                if (targetFishedListFrequency[targetLevelIndex] === 1) {
-                    targetFishedList.splice(targetLevelIndex, 1);
-                    targetFishedListFrequency.splice(targetLevelIndex, 1);
-                } else {
-                    targetFishedListFrequency[targetLevelIndex]--;
-                }
-
-                // Adding new levels
-                const userNewLevelIndex = userFishedList.findIndex(level => level === levelToGet);
-                const targetNewLevelIndex = targetFishedList.findIndex(level => level === levelToGive);
-
-                if (userNewLevelIndex === -1 ) {
-                    userFishedList.push(levelToGet);
-                    userFishedListFrequency.push(1);
-                } else {
-                    userFishedListFrequency[userNewLevelIndex]++;
-                }
-
-                if (targetNewLevelIndex === -1) {
-                    targetFishedList.push(levelToGive);
-                    targetFishedListFrequency.push(1);
-                } else {
-                    targetFishedListFrequency[targetNewLevelIndex]++;
-                }
-
-                await Promise.all([
-                    db[list].update({ fished_list: JSON.stringify(userFishedList), fished_list_frequency: JSON.stringify(userFishedListFrequency) }, { where: { user: user.id } }),
-                    db[list].update({ fished_list: JSON.stringify(targetFishedList), fished_list_frequency: JSON.stringify(targetFishedListFrequency) }, { where: { user: targetUser.id } }),
-                ]);
-
-                await interaction.editReply({components: [], embeds:[new EmbedBuilder().setDescription(`${tradeText}\n:white_check_mark: Trade successful`).setColor("Red")]});
+            
             } else if (tradeConfirmation.customId === 'cancelTrade') {
                 await interaction.editReply({components: [], embeds: [new EmbedBuilder().setDescription(`${tradeText}\n:x: Trade rejected`).setColor("Red")]});
             }
